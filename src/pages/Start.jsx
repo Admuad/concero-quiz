e732a5311b881d8cae3acef609b437f90a8b36e1
Muhammed Adediran <125326384+Admuad@users.noreply.github.com>
2025-11-10 10:15:50 +0100

    Fix navigation path to leaderboard

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -11,7 +11,7 @@ export default function Start() {
   const user = state?.user;
 
   const handleStart = () => {
-    if (user) navigate("/quiz", { state: { user } });
+    if (user) navigate("/leaderboar", { state: { user } });
     else navigate("/login");
   };
 
